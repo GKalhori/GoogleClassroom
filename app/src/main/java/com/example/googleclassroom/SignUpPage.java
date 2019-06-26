de086dfb691ced4646e438ef3a62066be86b496c
Reyhane <kharazmireyhaneh@gmail.com>
2019-06-26 19:09:21 +0430

    fourth commit

--- app/src/main/java/com/example/googleclassroom/SignUpPage.java
+++ app/src/main/java/com/example/googleclassroom/SignUpPage.java
@@ -2,12 +2,39 @@ package com.example.googleclassroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpPage extends AppCompatActivity {
+    EditText username, password;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        username = (EditText) findViewById(R.id.usernameText);
+        password = (EditText) findViewById(R.id.passwordText);
+        signUp = (Button) findViewById(R.id.loginButton);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if (username.getText().toString().length() == 0)
+                    username.setError("Please Enter Username!");
+
+                else if (password.getText().toString().length() == 0)
+                    username.setError("Please Enter Password!");
+
+                else {
+                    // do async task
+                    username.setError(null);
+                    password.setError(null);
+                }
+            }
+        });
     }
 }
