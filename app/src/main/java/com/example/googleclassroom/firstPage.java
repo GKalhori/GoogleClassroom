c7209dce11c2d26c136d0f9fe8147e42789964ad
Ghazal Kalhori <ghazalkalhori@yahoo.com>
2019-07-06 00:49:50 +0430

    ghazal commit

--- app/src/main/java/com/example/googleclassroom/firstPage.java
+++ app/src/main/java/com/example/googleclassroom/firstPage.java
@@ -16,44 +16,22 @@ import java.util.List;
 
 public class firstPage extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-
-    private List<ProductModel> listData = new ArrayList<ProductModel>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
 //        Toolbar toolbar = findViewById(R.id.toolbar) ;
 //        setSupportActionBar(toolbar);
-
-
 //        android.support.v7.app.ActionBar ab = getSupportActionBar();
 //       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar) ;
 //       // setSupportActionBar(toolbar);
 //        ab.setLogo(R.drawable.ic_launcher_background);
 //        ab.setDisplayUseLogoEnabled(true);
 //        ab.setDisplayShowHomeEnabled(true);
-
-
-        initData();
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-       // recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-       // adapter = new RecyclerViewAdapter();
-        recyclerView.setAdapter(adapter);
     }
 
-    private void initData() {
-        // here to describe
-        listData.add(new ProductModel(R.drawable.b1, "ap", "vahidi", 50));
-        listData.add(new ProductModel(R.drawable.b1, "math", "alavi", 30));
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,27 +45,21 @@ public class firstPage extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.more_vertt:
-                Toast.makeText(getApplicationContext(), "Welcome To About Us Page", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(firstPage.this, aboutUs.class);
-                startActivity(intent);
-                return true;
-            //  case R.id.add:
-            // Toast.makeText(getApplicationContext(),"you choosed add!",Toast.LENGTH_LONG).show();
-            // return true ;
+                startActivity(new Intent(firstPage.this, aboutUs.class));
+                break;
+
             case R.id.create:
                 Toast.makeText(getApplicationContext(), "Let's create class", Toast.LENGTH_LONG).show();
-                Intent intent1 = new Intent(firstPage.this, createClass.class);
-                startActivity(intent1);
-                return true;
+                startActivity(new Intent(firstPage.this, createClass.class));
+                break;
             case R.id.join:
                 Toast.makeText(getApplicationContext(), "Let's join class", Toast.LENGTH_LONG).show();
-                Intent intent2 = new Intent(firstPage.this, joinClass.class);
-                startActivity(intent2);
-                return true;
+                startActivity(new Intent(firstPage.this, joinClass.class));
+                break;
             default:
                 return super.onOptionsItemSelected(item);
 
         }
-
+        return super.onOptionsItemSelected(item);
     }
 }
