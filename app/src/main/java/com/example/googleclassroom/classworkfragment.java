d3548ba2b512bc02ddbdb83da4d9e30c15824ff4
Reyhane <kharazmireyhaneh@gmail.com>
2019-07-06 02:25:04 +0430

    20th commit

--- app/src/main/java/com/example/googleclassroom/classworkfragment.java
+++ app/src/main/java/com/example/googleclassroom/classworkfragment.java
@@ -15,32 +15,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import io.github.yavski.fabspeeddial.FabSpeedDial;
+//import io.github.yavski.fabspeeddial.FabSpeedDial;
 
 public class classworkfragment extends Fragment {
-    FabSpeedDial fabSpeedDial ;
+//    FabSpeedDial fabSpeedDial ;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_classwork, container, false);
-fabSpeedDial = (FabSpeedDial) view.findViewById(R.id.fabSpeedDial);
-fabSpeedDial.setMenuListener(new FabSpeedDial.MenuListener() {
-    @Override
-    public boolean onPrepareMenu(NavigationMenu navigationMenu) {
-        return true;
-    }
-
-    @Override
-    public boolean onMenuItemSelected(MenuItem menuItem) {
-        return true;
-    }
-
-    @Override
-    public void onMenuClosed() {
-
-    }
-});
-return view ;
+        return inflater.inflate(R.layout.fragment_classwork, container, false);
+//        FloatingActionButton fab = (FloatingActionButton)view.findViewById(R.id.myFAB);
+//        fab.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(),MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//fabSpeedDial = (FabSpeedDial) view.findViewById(R.id.fabSpeedDial);
+//fabSpeedDial.setMenuListener(new FabSpeedDial.MenuListener() {
+//    @Override
+//    public boolean onPrepareMenu(NavigationMenu navigationMenu) {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onMenuItemSelected(MenuItem menuItem) {
+//        return true;
+//    }
+//
+//    @Override
+//    public void onMenuClosed() {
+//
+//    }
+//});
+//return view ;
     }
 
     @Override
